a973e5de98c7b7a0c1a6ec8a2ed93721b5bc47e5
sakib3046 <sakib3046@gmail.com>
2024-05-09 20:44:00 +0600

    String functions

---
+++ basics/stringFunctions.py
@@ -0,0 +1,21 @@
+string1 = 'hello, My name is Md Sakib Hasan'
+print('The string is :',string1)
+
+#slicing
+print('sliced part is: ', string1[11:20])
+
+#endwith
+print('last charecter of the string is N:',string1.endswith('n'))
+
+#Capitalize
+print('After capitalize the string:',string1.capitalize())
+
+# replecment
+print('Sakib => Rakib:',string1.replace('Sakib','Rakib'))
+
+#finding 
+
+print('Finding S:',string1.find('S'))
+
+#counting
+print('The string have ',string1.count('s'),' times S')
