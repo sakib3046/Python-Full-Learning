635479d03fe1690cc8336e504b3073c1ded814f5
sakib3046 <sakib3046@gmail.com>
2024-05-22 15:41:17 +0600

    python loops basic

--- basics/dictionary.py
+++ basics/dictionary.py
@@ -1,24 +1,24 @@
-#like as JSON
-#list Mutable
+# like as JSON
+# list Mutable
 
-myInfo={
-    "name":"Md Sakib Hasan",
-    "age":21,
-    "isAdult":True,
-    "favFood":['rice','chiken']
+myInfo = {
+    "name": "Md Sakib Hasan",
+    "age": 21,
+    "isAdult": True,
+    "favFood": ['rice', 'chiken']
 }
-print('My name is',myInfo["name"])
-myInfo["surename"]='Halum'
-print('after adding surename the dictionary is:',myInfo)
+print('My name is', myInfo["name"])
+myInfo["surename"] = 'Halum'
+print('after adding surename the dictionary is:', myInfo)
 
-#nested dictionary
-subjects={
-    "math":{
-        '1st':85,
-        '2nd':90
+# nested dictionary
+subjects = {
+    "math": {
+        '1st': 85,
+        '2nd': 90
     }
 }
 
-print('Math 1st paper number',subjects["math"]["1st"])
+print('Math 1st paper number', subjects["math"]["1st"])
 
 print(subjects.keys())
