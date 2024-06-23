996e9b76018bd37da5793db4d8ba60483cb94f65
sakib3046 <sakib3046@gmail.com>
2024-06-23 17:17:19 +0600

    loops

---
+++ basics/loops.py
@@ -0,0 +1,14 @@
+# Just basic Syntax of python loop
+count = 0
+while count <= 5:
+    print("sakib")
+    count += 1
+
+# python for loop is like as each loop
+
+name = "sakib"
+
+for char in name:
+    print(char)
+else:
+    print("Nothing")
